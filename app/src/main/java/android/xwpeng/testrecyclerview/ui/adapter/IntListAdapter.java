fb48aedb7b96b05a399c4c924457145638cee703
xwpengroot <cocpl0101001@gamil.com>
2017-03-22 17:54:22 +0800

    侧滑探索，找到可行代码．今日暂停

--- app/src/main/java/android/xwpeng/testrecyclerview/ui/adapter/IntListAdapter.java
+++ app/src/main/java/android/xwpeng/testrecyclerview/ui/adapter/IntListAdapter.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.xwpeng.testrecyclerview.R;
+import android.xwpeng.testrecyclerview.util.PublicUtil;
+import android.xwpeng.testrecyclerview.view.SwipeMenuLayout5;
 
 import java.util.List;
 
@@ -35,20 +37,49 @@ public class IntListAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 //        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_int_list2, parent, false));
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_int_list4, parent, false));
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_int_list5, parent, false));
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
         final ViewHolder vh = (ViewHolder) holder;
         final int data = mData.get(position);
-        vh.contentView.setText(data + "");
-      /*  vh.itemView.setOnClickListener(new View.OnClickListener() {
+        vh.titleView.setText(data + "");
+        vh.contentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PublicUtil.showToast("click: " + data);
+                vh.swipeMenuLayout.setLeftSwipe(true);
+                PublicUtil.showToast("click data:" + data);
             }
-        });*/
+        });
+        vh.contentView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PublicUtil.showToast("long click data: " + data);
+                vh.swipeMenuLayout.setLeftSwipe(false);
+                return true;
+            }
+        });
+
+        vh.menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PublicUtil.showToast("menu1 data: " + data);
+            }
+        });
+        vh.menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PublicUtil.showToast("menu2 data: " + data);
+            }
+        });
+        vh.menu3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PublicUtil.showToast("menu3 data: " + data);
+            }
+        });
+
   /*      vh.deleteView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,18 +106,26 @@ public class IntListAdapter extends RecyclerView.Adapter {
 
     private class ViewHolder extends RecyclerView.ViewHolder {
         private View itemView;
-        private TextView contentView;
+        private View contentView;
         private TextView deleteView;
+        private TextView titleView;
+        private View menu1, menu2, menu3 ;
+        private SwipeMenuLayout5 swipeMenuLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
             this.itemView = itemView;
-            contentView = (TextView) itemView.findViewById(R.id.item_int_list_textview);
+            contentView = itemView.findViewById(R.id.item_content);
             deleteView = (TextView) itemView.findViewById(R.id.item_int_list_deleteview);
+            titleView = (TextView) itemView.findViewById(R.id.item_int_list_textview);
+            menu1 = itemView.findViewById(R.id.item_menu1);
+            menu2 = itemView.findViewById(R.id.item_menu2);
+            menu3 = itemView.findViewById(R.id.item_menu3);
+            swipeMenuLayout = (SwipeMenuLayout5)itemView.findViewById(R.id.item_swipe_menu_layout);
         }
     }
 
-    public interface CallBack{
+    public interface CallBack {
         void deleteItem(int position);
     }
 
